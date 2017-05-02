f944988651693a355822764bffe74b1a45794c2a
MateuszBieniek92 <bieniek.mateusz92@gmail.com>
2017-05-02 20:28:31 +0200

    Initial commit.

---
+++ dist/animation-debug.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    /// btn menu mobile    
+
+    var mobile = window.matchMedia("screen and (max-width: 550px)");
+    var desktop = window.matchMedia("screen and (min-width: 900px)");
+    var tablet = window.matchMedia("screen and (min-width: 551px) and (max-width: 899px)");
+    var $btn1 = $('.showHideMenu');
+    var $list = $('.menu');
+
+});
---
+++ dist/animation.js
@@ -0,0 +1,1 @@
+$(function(){window.matchMedia("screen and (max-width: 550px)"),window.matchMedia("screen and (min-width: 900px)"),window.matchMedia("screen and (min-width: 551px) and (max-width: 899px)"),$(".showHideMenu"),$(".menu")});
